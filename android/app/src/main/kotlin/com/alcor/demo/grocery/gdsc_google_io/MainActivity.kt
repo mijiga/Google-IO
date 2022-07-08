a9f6f3e0b5961f2d3126ddd003b11af6baf15a3a
Steve Chikwiri <37661480+mijiga@users.noreply.github.com>
2022-07-09 01:10:31 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/alcor/demo/grocery/gdsc_google_io/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.alcor.demo.grocery.gdsc_google_io
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Google I/O Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  final TextEditingController _inputController = TextEditingController();
+  List<String> groceries = ["Cooking Oil"];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Grocery List App"),
+      ),
+      body: Column(
+        children: [
+          //TextField
+          Container(
+            margin: const EdgeInsets.only(top: 16, left: 16, right: 16),
+              width: MediaQuery.of(context).size.width,
+              child: TextField(
+                controller: _inputController,
+              )),
+          //Grocery
+          Container(
+            margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+            width: MediaQuery.of(context).size.width,
+            height: 50,
+            child: ElevatedButton(onPressed: (){
+              setState(() {
+                String newGrocery = _inputController.text.toString();
+
+                if(newGrocery.isNotEmpty) {
+                  groceries.add(newGrocery);
+                }
+              });
+            }, child: const Text("Add New Item"),),
+          ),
+          //ListOfGroceries
+          Expanded(
+            child: ListView.builder(
+                itemCount: groceries.length,
+                itemBuilder: (context, index) {
+                  String _grocery = groceries[index];
+                  return Container(
+                    margin: const EdgeInsets.symmetric(vertical: 16, horizontal: 16),
+                      child: Text(_grocery));
+                }),
+          ),
+        ],
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
